21a85982ab161dff22dc0e40cb6c8df1ffea84b1
ynivermine92 <ynivermine92@gmail.com>
2020-12-02 21:24:04 +0200

    v1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let tabsFuction = function(){
+  let tabs = document.querySelectorAll('.tabs');
+  let tabsItem = document.querySelectorAll('.tab');
+  let tabName;
+  
+  tabs.forEach(item =>{
+    item.addEventListener('click',tabsBootm);
+  })
+   function tabsBootm(){
+     tabs.forEach(item=>{
+       item.classList.remove('avtive');
+     })
+        this.classList.add('acvive');
+        tabName =this.getAttribute('tab-name');
+        tabsActive(tabName);
+   }
+      function tabsActive(tabName){
+        tabsItem.forEach(item =>{
+          item.classList.contains(tabName)?item.classList.add('active'):
+          item.classList.remove('active')
+        })
+      }
+};
+tabsFuction();
+
+  
