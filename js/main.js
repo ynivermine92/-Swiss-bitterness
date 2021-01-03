665b2d45a2be60ed1aa5a7fdcfcfe8094888ee40
ynivermine92 <ynivermine92@gmail.com>
2021-01-03 18:08:11 +0200

    v5

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@ let tabsFuction = function(){
   let tabs = document.querySelectorAll('.tabs');
   let tabsItem = document.querySelectorAll('.tab');
   let tabCustomer = document.querySelectorAll(".customer-reviews__content");
-  let commentTabsActive = document.querySelector (".customer-reviews__tab-1.tabs");
-  let commentTabs = document.querySelector (".customer-reviews__tab-2.tabs");
+  let customerReviewsTabs = document.querySelector(".customer-reviews__tab-1.tabs-tab");
+  let tabsOn = document.querySelector(".customer-reviews__content.tabs-on-1"); 
+  let tabsTab = document.querySelectorAll('.tabs-tab');
   let tabName;
- 
-
-
+  let tabsName;
   
+
+
   tabs.forEach(item =>{
     item.addEventListener('click',tabsBootm);
   })
    function tabsBootm(){
      tabs.forEach(item=>{
-       item.classList.remove('avtive');
+       item.classList.remove('acvive');
      })
         this.classList.add('acvive');
         tabName =this.getAttribute('tab-name');
@@ -26,20 +27,40 @@ let tabsFuction = function(){
           item.classList.remove('active')
         })
       }
+     
+
+  tabsTab.forEach(item=>{
+   item.addEventListener('click',tabsTabActiv);
+  });
+
+ function tabsTabActiv(){
+  tabsTab.forEach(item=>{
+    item.classList.remove('acvive');
+    item.style.color="#c3c3c3";
+    item.style="background-color:#fff";
+ })
+    this.classList.add('acvive');
+    this.style.color="#f6f9f3";
+    this.style="background-color:#f6f9f3";
+    tabsName =this.getAttribute('tabs-name');
+    tabsActiveCustomer(tabsName);
+}
+function tabsActiveCustomer(customerTabs){
+  tabCustomer.forEach(item =>{
+    item.classList.contains(customerTabs)?item.classList.add('active'):
+    item.classList.remove('active')
+  })
+  tabCustomer.forEach(item =>{
+    item.classList.contains(customerTabs)?item.classList.add('active'):
+    item.classList.remove('active')
+  })
+}
+   function tabsTabInactive(){
+     customerReviewsTabs.style.color="#c3c3c3";
+      tabsOn.style.display = 'none'; 
+   }
+   document.querySelector('.customer-reviews__tab-2.tabs-tab').onclick=tabsTabInactive;  
+
+
 };
 tabsFuction();
-
-
-let tabavtive = function (e){
-   let tab = document.querySelector('.customer-reviews__tab-1');
-   let tabs = document.querySelector('.customer-reviews__tab-2');
-
-   function tabcComments (e){
-  
-   
-    console.log(e);
-   }
-   document.querySelector('.customer-reviews__tab-1,customer-reviews__tab-2').onclick = tabcComments;
-
-};
-tabavtive();
